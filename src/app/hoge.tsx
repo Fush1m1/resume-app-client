80aea28abfb1c6b94f7fba4fa9eb3f9b291375c9
Fush1m1 <onyx_1873@icloud.com>
2025-03-16 16:07:48 +0900

    add: api fetch

---
+++ src/app/hoge.tsx
@@ -0,0 +1,25 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+// API_URLをfetchしてresに格納
+export async function fetchHoge(): Promise<any> {
+  const res = await fetch(`https://${API_URL}`);
+  await res.json().then((data) => {
+    console.log(data);
+    return data;
+  });
+}
+
+var data: string | undefined;
+fetchHoge().then((fetchedData) => {
+  data = fetchedData;
+  console.log(data);
+});
+
+
+export function Hoge() {
+  return (
+    <div>
+      <h1>${data}</h1>
+    </div>
+  );
+}
