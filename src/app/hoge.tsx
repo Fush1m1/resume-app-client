6d361d229111607fdf6abe91d61a3e0c6a4b9493
Fush1m1 <onyx_1873@icloud.com>
2025-03-16 16:16:58 +0900

    fix:

--- src/app/hoge.tsx
+++ src/app/hoge.tsx
@@ -1,25 +1,28 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+// Define the response data type
+type ResponseData = {
+  message: string;
+};
+
 // API_URLをfetchしてresに格納
-export async function fetchHoge(): Promise<any> {
+export async function fetchHoge(): Promise<ResponseData> {
   const res = await fetch(`https://${API_URL}`);
-  await res.json().then((data) => {
-    console.log(data);
-    return data;
-  });
+  const data = await res.json();  // Await here instead of using .then
+  console.log(data);
+  return data;  // Return the data as ResponseData
 }
 
-let data: string | undefined;
+let data: ResponseData | undefined;
 fetchHoge().then((fetchedData) => {
   data = fetchedData;
   console.log(data);
 });
 
-
 export function Hoge() {
   return (
     <div>
-      <h1>${data}</h1>
+      <h1>{data?.message}</h1>  {/* Safely access the message */}
     </div>
   );
 }
